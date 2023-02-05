f756d287a5d8fc3ad8ca7fe156017a907c2c4534
Marco Di Spirito <marco.n.dispirito@gmail.com>
2023-02-04 22:38:10 -0800

    add starting project structure

---
+++ src/models/model.ts
@@ -0,0 +1,11 @@
+// import { Model } from 'yalps';
+
+// const model_DFS_NBA: Model = {
+//     direction: "maximize",
+//     objective: "fantasy_points",
+//     constraints: {
+//         salary: { max: 200 }
+//     },
+//     variables: ,
+//     binaries: true
+// }
---
+++ src/optimizer.ts
@@ -0,0 +1,16 @@
+
+class Optimizer {
+
+}
+
+/**
+ * decisions
+ * which players to include - position, salary, projection
+ * players included are either 0 or 1, included or not
+ */
+
+/** 
+ * constraints
+ * budget <= 200
+ * positional
+ */
