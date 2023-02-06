40bd7ba93104794445f58adf2d9c387ee88478c3
Marco Di Spirito <marco.n.dispirito@gmail.com>
2023-02-05 21:45:28 -0800

    begin csv parser

---
+++ src/parsers/csvParser.ts
@@ -0,0 +1,14 @@
+import { parse } from 'csv-parse/sync';
+import { promises as fs } from 'fs';
+
+export class CsvParser {
+
+    public static async parseCSV(filePath: string): Promise<any> {
+        const content = await fs.readFile(filePath);
+
+        return parse(content, {
+            columns: true,
+            skip_empty_lines: true
+        });
+    }
+}
