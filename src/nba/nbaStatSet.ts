cce3129900ea227ced236c8bc67bbe3906a8aa3d
Marco Di Spirito <marco.n.dispirito@gmail.com>
2023-02-25 21:00:00 -0800

    add validation when setting stat in constructor

--- src/nba/nbaStatSet.ts
+++ src/nba/nbaStatSet.ts
@@ -9,6 +9,7 @@ export class NBAStatSet {
         this.stats = new Map<string, Stat>();
 
         for (const [statName, statValue] of Object.entries(statProduction)) {
+            if (statValue < 0) throw new RangeError(`Statistic ${statName}: ${statValue} cannot be less than 0.`)
             this.stats.set(statName, {name: statName, value: statValue, enabled: true})
         }
     }
