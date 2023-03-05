b06564a15d81ffa88b9d54a15be21248bc0c6a1a
Marco Di Spirito <marco.n.dispirito@gmail.com>
2023-03-04 20:54:17 -0800

    add NBA DFS model

---
+++ src/models/NBADFSModel.ts
@@ -0,0 +1,31 @@
+import { Model } from 'yalps';
+import { Solution } from 'yalps';
+import { solve } from 'yalps';
+
+export interface NBADFSModelProps {
+    objective: string,
+    salaryLimit: number,
+    maxAvailableRosterSpots: number,
+    variables: Map<string, any>
+}
+
+export class NBADFSModel implements Model {
+    direction: "maximize" | "minimize" | undefined = "maximize";
+    objective: string;
+    constraints: { salary: { max: number }, availableRosterSpots: { max: number } };
+    variables: Map<string, any>;
+    binaries: boolean = true;
+
+    constructor(props: NBADFSModelProps) {
+        this.objective = props.objective;
+        this.variables = props.variables;
+        this.constraints = {
+            salary: { max: props.salaryLimit },
+            availableRosterSpots: { max: props.maxAvailableRosterSpots }
+        };
+    }
+
+    public getSolution(): Solution {
+        return solve(this);
+    }
+}
