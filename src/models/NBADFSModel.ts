196f457fa7c6e162530523da1c7b4d4e5b495385
Marco Di Spirito <37250241+mdispirito@users.noreply.github.com>
2023-04-01 15:16:36 -0700

    NBA DFS Model (#2)

---
+++ src/models/NBADFSModel.ts
@@ -0,0 +1,31 @@
+import { Coefficients, Model } from 'yalps';
+import { Solution } from 'yalps';
+import { solve } from 'yalps';
+
+export interface NBADFSModelProps {
+    objective: string,
+    salaryLimit: number,
+    maxAvailableRosterSpots: number,
+    variables: Map<string, any>
+}
+
+export class NBADFSModel implements Model {
+    direction: "maximize" | "minimize" | undefined = "maximize";
+    objective: string;
+    constraints: { salary: { max: number }, rosterSpot: { max: number } };
+    variables: Map<string, Coefficients>;
+    binaries: boolean = true;
+
+    constructor(props: NBADFSModelProps) {
+        this.objective = props.objective;
+        this.variables = props.variables;
+        this.constraints = {
+            salary: { max: props.salaryLimit },
+            rosterSpot: { max: props.maxAvailableRosterSpots }
+        };
+    }
+
+    public getSolution(): Solution {
+        return solve(this);
+    }
+}
---
+++ test/nbaStats.test.ts
@@ -0,0 +1,44 @@
+import { NBAStatSet } from "../src/nba/nbaStatSet.js";
+import { StatProductionType } from "../src/interfaces/FantasyStats.js";
+import { NBAPlayerProjection } from "../src/nba/nbaPlayerProjection.js";
+import { YAHOO_NBA_DFS_STAT_MAP } from "../src/nba/nbaFantasyStatValues.js";
+
+describe("NBA Stat Set", () => {
+    it("should store all stats that are passed in", () => {
+        const fortyPiece = new NBAStatSet(StatProductionType.PROJECTED, {
+            PTS: 40
+        });
+
+        const expected = {
+            name: "PTS",
+            value: 40,
+            enabled: true
+        }
+
+        expect(fortyPiece.stats.get("PTS")).toStrictEqual(expected);
+    });
+
+    it("should throw an error if it receives a negative stat", () => {
+        expect(() => {
+            new NBAStatSet(StatProductionType.PROJECTED, {
+                PTS: -10
+            })
+        }).toThrow(RangeError);
+    });
+});
+
+describe("NBA Player Projection", () => {
+    it("Should generate an exact fantasy output, given a stat set and stat map", () => {
+        const westbrickSpecial = new NBAStatSet(StatProductionType.PROJECTED, {
+            PTS: 16,
+            REB: 11,
+            AST: 12,
+            STL: 1,
+            BLK: 0,
+            TOV: 8
+        });
+
+        const westbrickSpecialFantasyOutput = new NBAPlayerProjection(westbrickSpecial, YAHOO_NBA_DFS_STAT_MAP);
+        expect(westbrickSpecialFantasyOutput.fantasyPoints).toBe(42.2);
+    });
+});
